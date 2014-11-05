53ecbbcad9c45f1659c0ecf577fb146184f4e7c9
Michael Rhodes <spam@michaelrhod.es>
2014-11-05 15:58:34 +1000

    1.0.2 Automatically convert Buffers to ArrayBuffers in the browser. This is required for create-torrent.

--- browser.js
+++ browser.js
@@ -6,5 +6,6 @@ delete window.Rusha
 module.exports = function(buf, raw) {
   var rusha = new Rusha 
   var method = raw ? 'rawDigest' : 'digest'
+  if (buf._isBuffer) buf = buf.toArrayBuffer()
   return rusha[method](buf)
 }
