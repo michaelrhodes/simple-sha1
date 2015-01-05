56e7303f0d17aa1842fe17f4cbdd5d0e3402b818
michaelrhodes <spam@michaelrhod.es>
2015-01-05 22:44:18 +1000

    Style changes.

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = sha1
-module.exports.sync = sha1Sync
-
 var crypto = require('crypto')
 
 function sha1 (buf, cb) {
-  var hash = sha1Sync(buf)
+  var hash = sha1sync(buf)
   process.nextTick(function () {
     cb(hash)
   })
 }
 
-function sha1Sync (buf) {
+function sha1sync (buf) {
   return crypto.createHash('sha1')
     .update(buf)
     .digest('hex')
 }
+
+module.exports = sha1
+module.exports.sync = sha1sync
