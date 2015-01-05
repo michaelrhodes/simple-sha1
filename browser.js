26bb93bf550e050fd428e261693f424bfcdd6ab4
Feross Aboukhadijeh <feross@feross.org>
2015-01-04 19:26:47 -0800

    Use single Rusha instance
    For the https://instant.io use case (BitTorrent piece hashing), I saw a
    10x perf improvement with this change.
    
    20s -> 2s

--- browser.js
+++ browser.js
@@ -3,8 +3,8 @@ var Rusha = require('rusha')
 // Rusha inserts a global for some reason.
 delete window.Rusha
 
+var rusha = new Rusha
 module.exports = function(buf, raw) {
-  var rusha = new Rusha 
   var method = raw ? 'rawDigest' : 'digest'
   if (buf._isBuffer) buf = buf.toArrayBuffer()
   return rusha[method](buf)
