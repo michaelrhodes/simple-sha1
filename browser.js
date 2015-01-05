f8ff95c517164f7731f26089c21b863c5b7b5670
Feross Aboukhadijeh <feross@feross.org>
2015-01-04 19:28:36 -0800

    Don't convert buffers
    This line is wrong for a couple reasons:
    
    - Checks private method of Buffer. Better to use Buffer.isBuffer (or
    https://github.com/feross/is-buffer if you don’t want to include the
    whole Buffer implementation in the bundle)
    - buf.toArrayBuffer() does an unnecessary copy
    
    Rusha can operate on Uint8Arrays just fine. Browser buffers are just
    Uint8Arrays, so there’s no problem here.

--- browser.js
+++ browser.js
@@ -6,6 +6,5 @@ delete window.Rusha
 var rusha = new Rusha
 module.exports = function(buf, raw) {
   var method = raw ? 'rawDigest' : 'digest'
-  if (buf._isBuffer) buf = buf.toArrayBuffer()
   return rusha[method](buf)
 }
